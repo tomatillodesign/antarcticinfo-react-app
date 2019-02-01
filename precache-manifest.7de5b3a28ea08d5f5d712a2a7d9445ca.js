b393f1d900241248b56afc0be86c1d38f7774c60
Chris Liu-Beers <tomatillodesign@users.noreply.github.com>
2019-02-01 11:40:44 -0500

    Updates

---
+++ precache-manifest.7de5b3a28ea08d5f5d712a2a7d9445ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/antarcticinfo-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "55faa35f46ffe1708931",
+    "url": "/antarcticinfo-react-app/static/js/runtime~main.55faa35f.js"
+  },
+  {
+    "revision": "1b4eefaeda544d5e4ee9",
+    "url": "/antarcticinfo-react-app/static/js/main.1b4eefae.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/antarcticinfo-react-app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1b4eefaeda544d5e4ee9",
+    "url": "/antarcticinfo-react-app/static/css/main.96665784.chunk.css"
+  },
+  {
+    "revision": "970f3cf29e1d04b09ced37e85d6fff9d",
+    "url": "/antarcticinfo-react-app/index.html"
+  }
+];
